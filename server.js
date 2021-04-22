a94157204920bf3d5184c86310ec995bd4e1856f
jvesche20 <70725626+jvesche20@users.noreply.github.com>
2021-04-21 21:15:23 -0400

    updates

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const mongoose = require('mongoose');
+const BodyParser = require('body-parser');
+const Mongoose = require('mongoose');
 
 const port = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/html/index.html`);
 });
+
+app.use(BodyParser.json());
 
 http.listen(port, () => {
   console.log(`running with port:${port}/`);
