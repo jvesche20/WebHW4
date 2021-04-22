21e9d8ebd37cb048cea54f7a0331d9b5f40cf3ff
jvesche20 <70725626+jvesche20@users.noreply.github.com>
2021-04-21 20:46:57 -0400

    created port

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+
+const port = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/html/index.html`);
+});
+
+http.listen(port, () => {
+  console.log(`running with port:${port}/`);
+});
