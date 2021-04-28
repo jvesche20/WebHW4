2e290f00cf0263575fbe20033e6c77198e94866b
jvesche20 <70725626+jvesche20@users.noreply.github.com>
2021-04-27 23:41:44 -0400

    final

--- database/find.js
+++ database/find.js
@@ -5,6 +5,5 @@ module.exports = async function f1(query) {
     return [];
   }
   const test = await Name.find({ name: { $regex: `^${query}` } }).select('-_id -__v');
-  const arrayOfStrings = test.map((entry) => entry.name);
-  return arrayOfStrings;
+  return test.map((entry) => entry.name);
 };
